f43c04d78a92c4b7017eff3ccacf283b3aae1d10
Julius Nyule <jlsnyule@yahoo.com>
2018-03-26 15:41:22 +0300

    add code for test testPrimeNumbers.py

--- primeNumbers.py
+++ primeNumbers.py
@@ -8,32 +8,25 @@ Eg 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31
 # Python Program - Print Prime Numbers
 
 #import math
-def isprime(n):
-    '''check if integer n is a prime'''
-
-    # make sure n is a positive integer
-    n = abs(int(n))
-
-    # 0 and 1 are not primes
-    if n < 2:
-        return []
-
-    # 2 is the only even prime number
-    if n == 2: 
-        return True    
-
-    # all other even numbers are not primes
-    if not n & 1: 
-        return False
-
-    # range starts with 3 and only needs to go up 
-    # the square root of n for all odd numbers
-    for x in range(3, int(n**0.5) + 1, 2):
-        if n % x == 0:
-            return False
-
-    return True
+def primeNumbers(n):
     
-    # test code here
-print(isprime(2)) # True
-print(isprime(-1)) # False
+    
+    prime_list = []
+    
+    for i in range(2,n+1):
+        
+        primeflag = True
+        
+        for num in prime_list:
+            
+            if(i%num ==0):
+                
+                primeflag=False
+                
+        if(primeflag):
+            
+            prime_list.append(i)
+            
+    return prime_list
+
+print(primeNumbers(10))
