10ccc36c608dc6577fe69cbf8d0d1ae519a30f7d
Julius Nyule <jlsnyule@yahoo.com>
2018-03-26 08:36:18 +0300

    add implementation code

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,37 @@
 # implementation
+'''
+A prime number is only evenly divisible by itself and 1.
+Note that 1 is not a prime number. create a function that generates prime numbers.
+Eg 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31
+'''
+
+# Python Program - Print Prime Numbers
+
+#import math
+def isprime(n):
+    '''check if integer n is a prime'''
+
+    # make sure n is a positive integer
+    n = abs(int(n))
+
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+
+    # 2 is the only even prime number
+    if n == 2: 
+        return True    
+
+    # all other even numbers are not primes
+    if not n & 1: 
+        return False
+
+    # range starts with 3 and only needs to go up 
+    # the square root of n for all odd numbers
+    for x in range(3, int(n**0.5) + 1, 2):
+        if n % x == 0:
+            return False
+
+    return True
+    
+print(isprime(2))
