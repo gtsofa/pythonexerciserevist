c25408d3ebb573de7007423a6724b8c97f2b468f
Julius Nyule <jlsnyule@yahoo.com>
2018-03-27 16:06:47 +0300

    modified

--- listcom/alist.py
+++ listcom/alist.py
@@ -1,6 +1,13 @@
 # alist.py
 # assume we want to create a list of squares.
-# range(10) using list comprehensions
+# range(2,10) using list comprehensions
 
+# we could use loops like such
+alist = []
+for i in range(2,10):
+  alist.append(i**2)
+  print(alist)
+  
+# using list comprehensions
 alist = [i**2 for i in range(2,10)]
 print(alist)
