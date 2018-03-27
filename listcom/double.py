f24a67f886a3734e25ceb14e8ff8f30cf4f371a1
Julius Nyule <jlsnyule@yahoo.com>
2018-03-27 16:33:33 +0300

    refactor code

--- listcom/double.py
+++ listcom/double.py
@@ -5,3 +5,9 @@ def double(numero):
   return numero**2
   
 print(double(3))
+
+# We can easily use list comprehension on that function.
+
+result = [double(numero) for numero in range(10)]
+
+print(result)
